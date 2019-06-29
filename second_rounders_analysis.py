c9416f6a579e5d10f5771c4061d218e3d37689e5
Kunal <kunaldutta1749@gmail.com>
2019-06-29 02:08:01 -0700

    determined average stats for 2nd round picks

---
+++ second_rounders_analysis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from datascience import *
+import numpy as np
+
+# access the csv file
+picks_data = Table.read_table("second_rounders_2004-14.csv")
+
+# General analysis
+avg_yrs = round(np.mean(picks_data.column('Yrs')))
+avg_games = round(np.mean(picks_data.column('G')))
+avg_pts = round(np.mean(picks_data.column('PTS_per_G')), 1)
+avg_vorp = round(np.mean(picks_data.column('VORP')), 1)
+print("The average second rounder will have a " + str(avg_yrs) + " year career.")
+print("The average second rounder will play " + str(avg_games) + " games in their career.")
+print("The average second rounder score " + str(avg_pts) + " points per game in their career.")
+print("The average second rounder brings " + str(avg_vorp) + " value over replacement player (VORP) throughout their career.")
