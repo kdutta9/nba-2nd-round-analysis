6439c667ff74158b1b6216d539ef00e3b0488580
Kunal <kunaldutta1749@gmail.com>
2019-07-08 15:52:21 -0700

    added beginning of per year analysis

--- second_rounders_analysis.py
+++ second_rounders_analysis.py
@@ -16,3 +16,9 @@ print("The average second rounder will have a " + str(avg_yrs) + " year career."
 print("The average second rounder will play " + str(avg_games) + " games in their career.")
 print("The average second rounder score " + str(avg_pts) + " points per game in their career.")
 print("The average second rounder brings " + str(avg_vorp) + " value over replacement player (VORP) throughout their career.")
+
+# average games per draft year
+for i in range(2005, 2015):
+    year_data = picks_data.where("Draft Year", are.equal_to(i))
+    draftyr_yrs = round(np.mean(year_data.column('G')))
+    print(str(i) + " draftees played an average of " + str(draftyr_yrs) + " games in their career.")
